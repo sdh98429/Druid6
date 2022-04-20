702ec79c867661bbf44e674cb4def7ef2d04edc2
DESKTOP-KVCQHCD\SSAFY <shaian2@naver.com>
2022-04-20 17:29:57 +0900

    feat: 서버 모니터링 페이지 제작

---
+++ frontend/druid6/src/ServerMonitoring/ServerMonitoring.jsx
@@ -0,0 +1,35 @@
+import './ServerMonitoring.css';
+import { Grid } from '@mui/material';
+// import { flexbox } from '@mui/system';
+import Box from '@mui/material/Box';
+
+
+export default function ServerMonitoring() {
+  return (
+    <div className='Container'>
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <h1 className='Header'>Server Workload</h1>
+        <h1 className='Header'>13:44</h1>
+      </Box>
+      <Grid container spacing={3}>
+        <Grid item xs={4}>
+          <div className='Comp'></div>
+        </Grid>
+        <Grid item xs={4}>
+          <div className='Comp'>
+            <h2 className='Comp-h2'>CPU</h2>
+          </div>
+        </Grid>
+        <Grid item xs={4}>
+          <div className='Comp'></div>
+        </Grid>
+        <Grid item xs={4}>
+          <div className='Comp'></div>
+        </Grid>
+        <Grid item xs={8}>
+          <div className='Comp'></div>
+        </Grid>
+      </Grid>
+    </div>
+  )
+}
