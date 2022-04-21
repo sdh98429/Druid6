9ea0a5cf0f3f2d68fb2c968ee0300e18fef740e8
yunoh <orobot13@gmail.com>
2022-04-21 11:45:59 +0900

    refactor : 람다함수 함수로 처리

--- frontend/druid6/src/StressTest/StressTest.jsx
+++ frontend/druid6/src/StressTest/StressTest.jsx
@@ -15,57 +15,79 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
+const handleClickStressTest = () => {
+  alert("부하 테스트 시작");
+};
+
+const handleClickGenerateScenario = () => {
+  alert("시나리오 시작");
+};
+
 export default function DirectionStack() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-    <Grid container spacing={3}>
-      <Grid item xs={6}>
-        <Item>    <Stack direction="column" spacing={2}>
-            <TextField id="outlined-basic" label="Value1" variant="outlined" />
-            <TextField id="outlined-basic" label="Value2" variant="outlined" />
-            <TextField id="outlined-basic" label="Value3" variant="outlined" />
-            <TextField id="outlined-basic" label="Value4" variant="outlined" />
-          </Stack>
+      <Grid container spacing={3}>
+        <Grid item xs={6}>
           <Item>
-            <Button
-              variant="contained"
-              onClick={() => {
-                alert("clicked");
-              }}
-            >
-              시나리오
-            </Button>
-          </Item></Item>
+            {" "}
+            <Stack direction="column" spacing={2}>
+              <TextField
+                id="outlined-basic"
+                label="Value1"
+                variant="outlined"
+              />
+              <TextField
+                id="outlined-basic"
+                label="Value2"
+                variant="outlined"
+              />
+              <TextField
+                id="outlined-basic"
+                label="Value3"
+                variant="outlined"
+              />
+              <TextField
+                id="outlined-basic"
+                label="Value4"
+                variant="outlined"
+              />
+            </Stack>
+            <Item>
+              <Button
+                variant="contained"
+                onClick={handleClickGenerateScenario()}
+              >
+                시나리오
+              </Button>
+            </Item>
+          </Item>
+        </Grid>
+        <Grid item xs={6}>
+          <Item>
+            <Stack direction="column" spacing={2}>
+              <Box
+                component="img"
+                sx={{
+                  height: 233,
+                  width: 350,
+                  maxHeight: { xs: 233, md: 167 },
+                  maxWidth: { xs: 350, md: 250 },
+                }}
+                alt="The house from the offer."
+                src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"
+              />
+              <item>
+                <Button
+                  variant="contained"
+                  onClick={handleClickStressTest}
+                >
+                  부하테스트 실시
+                </Button>
+              </item>
+            </Stack>
+          </Item>
+        </Grid>
       </Grid>
-      <Grid item xs={6}>
-        <Item><Stack direction="column" spacing={2}>
-            <Box
-              component="img"
-              sx={{
-                height: 233,
-                width: 350,
-                maxHeight: { xs: 233, md: 167 },
-                maxWidth: { xs: 350, md: 250 },
-              }}
-              alt="The house from the offer."
-              src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"
-            />
-            <item>
-            <Button
-              variant="contained"
-              onClick={() => {
-                alert("clicked");
-              }}
-            >
-                
-              부하테스트 실시
-            </Button>
-            </item>
-          </Stack></Item>
-      </Grid>
-     
-    </Grid>
-  </Box>
-    
+    </Box>
   );
 }
