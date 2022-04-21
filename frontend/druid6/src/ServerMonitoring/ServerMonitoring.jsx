308341da1e1d13b9405b646d6d886892b3330df8
DESKTOP-KVCQHCD\SSAFY <shaian2@naver.com>
2022-04-21 11:30:04 +0900

    chore: css 클래스 네임 소문자로 수정

--- frontend/druid6/src/ServerMonitoring/ServerMonitoring.jsx
+++ frontend/druid6/src/ServerMonitoring/ServerMonitoring.jsx
@@ -6,10 +6,10 @@ import Box from '@mui/material/Box';
 
 export default function ServerMonitoring() {
   return (
-    <div className='Container'>
+    <div className='container'>
       <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <h1 className='Header'>Server Workload</h1>
-        <h1 className='Header'>13:44</h1>
+        <h1 className='header'>Server Workload</h1>
+        <h1 className='header'>13:44</h1>
       </Box>
       <Grid container spacing={3}>
         <Grid item xs={4}>
