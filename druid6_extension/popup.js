5891daef0f13bcb80ee222266ad9cb9118b4a9eb
onys <thkim2017@gmail.com>
2022-04-27 16:24:35 +0900

    feat: 🎸 옵션 페이지 생성 및 input 입력 콘솔에 테스트

--- druid6_extension/popup.js
+++ druid6_extension/popup.js
@@ -1,4 +1,9 @@
 const timeElement = document.getElementById("time");
 const currentTime = new Date().toLocaleTimeString();
 timeElement.textContent = `현재 시각은 ${currentTime} 입니다.`;
-console.log(currentTime);
+
+chrome.action.setBadgeText({
+  text: "TIME",
+}, () => {
+  console.log("Finishied setting badge text")
+})
