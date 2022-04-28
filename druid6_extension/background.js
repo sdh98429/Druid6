514b975f5b93f987c9299b147f273f7301e671f9
onys <thkim2017@gmail.com>
2022-04-28 11:34:37 +0900

    feat: 🎸 background worker, chrome API 이용하여 실시간 시간 띄우기

---
+++ druid6_extension/background.js
@@ -0,0 +1,15 @@
+chrome.alarms.create({
+  periodInMinutes: 1 / 60,
+});
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+  chrome.storage.local.get(["timer"], res => {
+    const time = res.timer ?? 0
+    chrome.storage.local.set({
+      timer: time + 1,
+    })
+    chrome.action.setBadgeText({
+      text: `${time + 1}`
+    })
+  })
+});
--- druid6_extension/popup.js
+++ druid6_extension/popup.js
@@ -1,8 +1,18 @@
 const timeElement = document.getElementById("time");
 const nameElement = document.getElementById("name");
+const timerElement = document.getElementById("timer");
 
-const currentTime = new Date().toLocaleTimeString();
-timeElement.textContent = `현재 시각은 ${currentTime} 입니다.`;
+function updateTimeElements() {
+  chrome.storage.local.get(["timer"], res => {
+    const time = res.timer ?? 0;
+    timerElement.textContent = `현재 초는 ${time}초 입니다.`;
+  });
+  const currentTime = new Date().toLocaleTimeString();
+  timeElement.textContent = `현재 시각은 ${currentTime} 입니다.`;
+}
+
+updateTimeElements();
+setInterval(updateTimeElements, 1000);
 
 chrome.action.setBadgeText({
   text: "TIME",
