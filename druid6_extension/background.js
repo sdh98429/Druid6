125b1c4e8758a5fce992e8f5432098a15724ccfe
onys <thkim2017@gmail.com>
2022-04-28 13:54:19 +0900

    feat: 🎸 10초마다 chrome notification 알림 뜨게 하기 but 지금 로컬에선 동작x

--- druid6_extension/background.js
+++ druid6_extension/background.js
@@ -8,8 +8,15 @@ chrome.alarms.onAlarm.addListener((alarm) => {
     chrome.storage.local.set({
       timer: time + 1,
     })
+
     chrome.action.setBadgeText({
       text: `${time + 1}`
     })
+    if (time % 10 === 0) {
+      this.registration.showNotification("Druid6 타이머 익스텐션", {
+        body: "10초가 지났어요!",
+        icon: "icon.png",
+      });
+    }
   })
 });
