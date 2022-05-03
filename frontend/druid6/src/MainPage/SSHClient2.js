09108ff3ce7d22cd5dfecd45d8385aff42561cac
kenny <kenny397@naver.com>
2022-05-03 13:48:26 +0900

    feat : ssh클라이언트 정보 출력

--- frontend/druid6/src/MainPage/SSHClient2.js
+++ frontend/druid6/src/MainPage/SSHClient2.js
@@ -3,26 +3,58 @@ const { readFileSync } = require('fs');
 const { Client } = require('ssh2');
 
 const conn = new Client();
-let result;
+let resultCpu;
+let resultDisk;
+let resultMemory;
 conn.on('ready', () => {
   console.log('Client :: ready2');
   setInterval(function (){
-    conn.exec(`free\n mpstat | tail -1 | awk '{print 100-$NF}\n '
-    `, (err, stream) => {
+    conn.exec(`free -m | grep Mem | awk '{print (($2-$6)/$2)*100}'`
+    , (err, stream) => {
       if (err) throw err;
       stream.on('close', (code, signal) => {
         //console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
         //conn.end();
       }).on('data', (data) => {
-        result=data;
-        console.log('STDOUT: ' + data);
+        resultMemory=data;
+        console.log('MEMORY: ' + data);
       }).stderr.on('data', (data) => {
         console.log('STDERR: ' + data);
       });
     });
-  },5000)
+  },1000)
+  setInterval(function (){
+    conn.exec(`df . | grep /dev | awk '{print $5}'`
+    , (err, stream) => {
+      if (err) throw err;
+      stream.on('close', (code, signal) => {
+        //console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
+        //conn.end();
+      }).on('data', (data) => {
+        resultDisk=data;
+        console.log('DISK: ' + data);
+      }).stderr.on('data', (data) => {
+        console.log('STDERR: ' + data);
+      });
+    });
+  },1000)
+  setInterval(function (){
+    conn.exec(`top -b -n 1 | grep -Po '[0-9.]+ id' | head -1 | awk '{print 100-$1}'`
+    , (err, stream) => {
+      if (err) throw err;
+      stream.on('close', (code, signal) => {
+        //console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
+        //conn.end();
+      }).on('data', (data) => {
+        resultCpu=data;
+        console.log('CPU: ' + data);
+      }).stderr.on('data', (data) => {
+        console.log('STDERR: ' + data);
+      });
+    });
+  },1000)
 }).connect({
-        host: "3.38.101.66",
+        host: "k6s2041.p.ssafy.io",
         username: "ubuntu",
         port: 22,
         privateKey : readFileSync('./K6S204T.pem'),
