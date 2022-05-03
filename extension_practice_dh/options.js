8b2cd4507afbff8a35348b17d20c08cbbfcd3e1d
SSAFY <sdh98429@naver.com>
2022-05-03 16:20:27 +0900

    feat: 🎸 Timer 예제 시작
    로컬 스토리지 저장

---
+++ extension_practice_dh/options.js
@@ -0,0 +1,15 @@
+const nameInput = document.getElementById("name-input")
+const saveBtn = document.getElementById("save-btn")
+
+saveBtn.addEventListener("click", () =>{
+  const name = nameInput.value
+  chrome.storage.sync.set({
+    name,
+  }, () => {
+    console.log(`Name is set to ${name}`)
+  })
+})
+
+chrome.storage.sync.get(["name", "test"], (res) => {
+  nameInput.value = res.name ?? "???"
+})
