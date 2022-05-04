828ee2a3873d2d513c58a63a75705aa2492ba20b
SSAFY <sdh98429@naver.com>
2022-05-04 15:53:22 +0900

    feat: 🎸 타이머 배지 시간 표시 및 시작, 중지, 리셋 버튼 추가

--- extension_practice_dh/options.js
+++ extension_practice_dh/options.js
@@ -1,15 +1,23 @@
 const nameInput = document.getElementById("name-input")
+const timeInput = document.getElementById("time-input")
 const saveBtn = document.getElementById("save-btn")
 
 saveBtn.addEventListener("click", () =>{
   const name = nameInput.value
+  const notificationTime = timeInput.value
   chrome.storage.sync.set({
     name,
+    notificationTime
   }, () => {
     console.log(`Name is set to ${name}`)
   })
 })
 
-chrome.storage.sync.get(["name", "test"], (res) => {
+chrome.storage.sync.get(["name", "notificationTime"], (res) => {
   nameInput.value = res.name ?? "???"
+  timeInput.value = res.notificationTime ?? 1000
 })
+
+setInterval(() => {
+  console.log("options")
+}, 1000)
--- extension_practice_dh/popup.js
+++ extension_practice_dh/popup.js
@@ -1,15 +1,45 @@
 const timeElement = document.getElementById("time")
 const nameElement = document.getElementById("name")
-const currentTime = new Date().toLocaleTimeString()
-timeElement.textContent = `The time is : ${currentTime}`
+const timerElement = document.getElementById("timer")
 
-chrome.action.setBadgeText({
-  text: "Time",
-}, () => {
-  console.log("Finished setting badge text.")
-})
+function updateTimeElements(){
+  console.log("popup")
+  chrome.storage.local.get(["timer"], (res) => {
+    const time = res.timer ?? 0
+    timerElement.textContent = `The timer is at : ${time} seconds`
+  })
+  const currentTime = new Date().toLocaleTimeString()
+  timeElement.textContent = `The time is : ${currentTime}`
+}
+
+updateTimeElements()
+setInterval(updateTimeElements, 1000)
+
 
 chrome.storage.sync.get(["name"], (res) =>{
   const name = res.name ?? "???"
   nameElement.textContent = `Your name is : ${name}`
+})
+
+const startBtn = document.getElementById("start")
+const stopBtn = document.getElementById("stop")
+const resetBtn = document.getElementById("reset")
+
+startBtn.addEventListener("click", () => {
+  chrome.storage.local.set({
+    isRunning: true,
+  })
+})
+
+stopBtn.addEventListener("click", () => {
+  chrome.storage.local.set({
+    isRunning: false,
+  })
+})
+
+resetBtn.addEventListener("click", () => {
+  chrome.storage.local.set({
+    timer: 0,
+    isRunning: false,
+  })
 })
