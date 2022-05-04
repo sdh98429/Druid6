50fc20d14d0151914d250b76b8170c29eb49bbe1
Leo <guswhddlf11@gmail.com>
2022-05-04 13:34:13 +0900

    feat: 🎸 webworker commit

--- webworker/src/myWorker.js
+++ webworker/src/myWorker.js
@@ -2,42 +2,6 @@
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default() => {
-  // fetch("https://www.naver.com", {
-  //         method: "get",
-  //         mode: 'no-cors'
-  //       })
-  // .then(function(response) {
-  //     return response.json()
-  // })
-  // .then(response => {
-  //   postMessage(response)
-  // })
-  // .catch(function(error) {
-  //     postMessage(error)
-  // });
-  // self.onmessage = async function( e ) {
-    
-  //   for (let i = 0; i < e.data.duration; i++) {
-  //     fetch(e.data.url, {
-  //       method: e.data.method,
-  //       headers: {
-  //         'Content-type': 'application/json'
-  //       },
-  //       body: JSON.stringify(
-  //         e.data.body
-  //       )
-  //     })
-  //     .then(response => {
-  //       return response.json()
-  //     })
-  //     .then(response => {
-  //       postMessage(response)
-  //     })
-  //     .catch(error => {
-  //       postMessage(error)
-  //     });
-  //   };
-  // }
 
   self.onmessage = async function( e ) {
     const response = await something(e)
