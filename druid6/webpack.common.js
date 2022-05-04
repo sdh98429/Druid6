e27df1cdf303caebf1f117199b3701b5c5720c17
onys <thkim2017@gmail.com>
2022-05-04 14:47:17 +0900

    feat: 🎸 build, start 명령 구분. build가 더 빠르지만 개발용

--- druid6/webpack.common.js
+++ druid6/webpack.common.js
@@ -1,10 +1,10 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 const HtmlPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  devtool: 'cheap-module-source-map',
   entry: {
     popup: path.resolve('src/popup/popup.tsx'),
     options: path.resolve('src/options/options.tsx'),
@@ -29,6 +29,9 @@ module.exports = {
     ]
   },
   plugins: [
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false,
+    }),
     new CopyPlugin({
       patterns: [
         {
