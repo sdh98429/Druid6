aedd2f4b03f6ffd6ba2b04ceb809c6b04a7a6c5a
onys <thkim2017@gmail.com>
2022-05-04 14:27:23 +0900

    feat: 🎸 react18에 맞게 renderDOM대신 creatRoot 사용

--- druid6/src/options/options.tsx
+++ druid6/src/options/options.tsx
@@ -1,9 +1,9 @@
 import React from "react"
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import './options.css'
 
 const test = <img src="icon.png" alt="icon-img" />
 
 const root = document.createElement('div')
 document.body.appendChild(root)
-ReactDOM.render(test, root)
+ReactDOM.createRoot(root).render(test)
