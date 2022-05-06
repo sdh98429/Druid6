7863a169fd2162621a01f234ac1757e5d3e462fb
yunoh <orobot13@gmail.com>
2022-05-06 18:11:55 +0900

    feat : create electron + react + express test app

---
+++ ReactJS-ElectronJS-ExpressJS-App/README.md
@@ -0,0 +1,32 @@
+# Getting Started with Electron,React,Express App
+
+## Available Scripts
+
+First In the project directory, you run:
+
+### `npm install`
+
+This install ElectronJS and ReactJS
+Then cd into new directory api
+
+### `cd api`
+
+Now Install The API dependencies.
+
+### `npm install`
+
+### `In api/routes/weather.js file line 3 set const API_KEY = with your API Key from weatherapi.com`
+
+### `npm start`
+
+To Test use npm start in the api folder. Open [http://localhost:7744](http://localhost:7744) to view it in your browser To make sure API is Running. You will have to kill process ID for port 7744 on your system in order to run later from the project root. you can run `concurrently --kill-others`
+
+### `cd ..`
+
+To go Back to Root of Project
+
+### `npm run dev`
+
+Build and Runs the ElectronJS App, ReactJS, and ExpressJS all at once
+
+### `IF YOU GET THE LENT ERROR CLOSE THE APPLICATION AND RUN 'npm run dev' again`
---
+++ ReactJS-ElectronJS-ExpressJS-App/api/routes/weather.js
@@ -0,0 +1,30 @@
+var express = require("express");
+const https = require('https');
+const API_KEY = "276927965e60f73c7a01d3e5b8ae6222";
+
+var router = express.Router();
+router.get("/:zipcode", function(req, res, next) {
+    https.get('https://api.weatherapi.com/v1/forecast.json?key='+API_KEY+'&q='+req.params.zipcode+'&days=4&aqi=no&alerts=no', (resp) => {
+    let data = '';
+    resp.on('data', (chunk) => {
+        data += chunk;
+    });
+    resp.on('end', () => {
+        if(JSON.parse(data).error){
+            res.statusCode = 401;
+            res.send(JSON.parse(data));
+            console.log("BAD INPUT STRING: "+JSON.parse(data).error.message);
+        }else{
+            console.log(data);
+            res.send(data); 
+        }
+    });
+    }).on("error", (err) => {
+        res.statusCode = 401;
+        res.send('None shall pass');
+        console.log("Error: " + err.message);
+    });
+    
+});
+
+module.exports = router;
