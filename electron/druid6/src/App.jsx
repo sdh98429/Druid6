f7ee2ceeec8b68008de932bac28868f7867f0383
onys <thkim2017@gmail.com>
2022-05-09 16:15:02 +0900

    feat: 🎸 router 적용 HashRouter, instead of browerRouter

--- electron/druid6/src/App.jsx
+++ electron/druid6/src/App.jsx
@@ -1,15 +1,20 @@
 import './App.css';
+
+import { HashRouter, Route } from 'react-router-dom';
+
 import SideBar from './SideBar';
 import Intro from './Intro';
 import ServerMonitoring from './ServerMonitoring';
 
 function App() {
   return (
-    <div>
-      <Intro />
-      <ServerMonitoring />
-      <SideBar />
-    </div>
+    <HashRouter>
+      <div>
+        <SideBar />
+        <Route path="/" exact component={Intro} />
+        <Route path="/server-monitoring" component={ServerMonitoring} />
+      </div>
+    </HashRouter>
   );
 }
 
