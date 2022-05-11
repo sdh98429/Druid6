98c0c6cc10a073a39d70ca683796ba4c1c19ed74
onys <thkim2017@gmail.com>
2022-05-11 10:37:34 +0900

    feat: 🎸 앵무새 돌아가는 각도 좀 더 다이나믹하게 변경

--- electron/druid6/src/pages/intro/Intro.jsx
+++ electron/druid6/src/pages/intro/Intro.jsx
@@ -15,8 +15,8 @@ export default function Intro() {
     const height = window.innerHeight;
     const width = window.innerWidth;
     // Creates angles of (-20, -20) (left, bottom) and (20, 20) (right, top)
-    const yAxisDegree = event.pageX / width * 40 - 20;
-    const xAxisDegree = event.pageY / height * -1 * 40 + 20;
+    const yAxisDegree = event.pageX / width * 60 - 20;
+    const xAxisDegree = event.pageY / height * -1 * 60 + 20;
     const target = document.getElementsByClassName('intro-logo')[0];
     target.style.transform = `rotateY(${yAxisDegree}deg) rotateX(${xAxisDegree}deg)`;
     // Set the sheen position
