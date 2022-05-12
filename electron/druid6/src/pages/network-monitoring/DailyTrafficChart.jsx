0853f4f409fda17e9030824744fe035c86814cd3
onys <thkim2017@gmail.com>
2022-05-12 22:18:42 +0900

    feat: 🎸 일별 트래픽 그래프만 구현

--- electron/druid6/src/pages/network-monitoring/DailyTrafficChart.jsx
+++ electron/druid6/src/pages/network-monitoring/DailyTrafficChart.jsx
@@ -1,7 +1,77 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import { faker } from '@faker-js/faker';
+
+import './DailyTrafficChart.scss';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  maintainAspectRatio: false,
+  plugins: {
+    title: {
+      display: false,
+    },
+  },
+  responsive: true,
+  scales: {
+    x: {
+      stacked: true,
+    },
+    y: {
+      stacked: true,
+    },
+  },
+};
+
+function range(start, count) {
+  let array = [];
+  while(count--) {
+    array.push(start++);
+  }
+  return array;
+}
+
+const labels = range(1, 31);
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Transmit',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: 'rgb(255, 99, 132)',
+    },
+    {
+      label: 'Receive',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: 'rgb(75, 192, 192)',
+    },
+  ],
+};
+
 export default function DailyTrafficChart() {
   return (
-    <div>
-      chart
+    <div className='DailyTrafficChart'>
+      <Bar
+        options={options} 
+        data={data}
+      />
     </div>
   );
 }
