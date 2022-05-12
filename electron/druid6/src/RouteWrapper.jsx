a393da0d01a78f3bdecff2eac70d4106bb063b78
onys <thkim2017@gmail.com>
2022-05-12 09:20:30 +0900

    feat: 🎸 route wrapper 구현 및 intro component 적용

--- electron/druid6/src/RouteWrapper.jsx
+++ electron/druid6/src/RouteWrapper.jsx
@@ -3,7 +3,9 @@ import './RouteWrapper.scss';
 export default function RouteWrapper({ Component }) {
   return (
     <div className="route-wrapper">
-      <Component />
+      <div className="route-container">
+        <Component />
+      </div>
     </div>
   );
 }
