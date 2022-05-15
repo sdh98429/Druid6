7cc3be005844ded52c5e27c6d668707778d95c52
DESKTOP-KVCQHCD\SSAFY <shaian2@naver.com>
2022-05-15 23:20:58 +0900

    feat: 자식컴포넌트 파일 생성

---
+++ electron/druid6/src/pages/web-performance/Solutions.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Message() {
+  return (
+    <p>안녕하세요? 렌더링 됐나요?</p>
+    // if(this.props.age >= 14) {
+    //     return <p>{`다음 단계로 진행해주세요`}</p>
+    // }else {
+    //     return <p>{`14세 미만은 부모님 동의가 필요합니다.`}</p>
+    // }
+  );
+}
