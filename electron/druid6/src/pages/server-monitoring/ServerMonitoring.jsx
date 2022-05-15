f0293f609741467bd5bcf70fec7334245a0d0f02
kenny <kenny397@naver.com>
2022-05-15 22:00:53 +0900

    feat : 서버모니터링과 로그인화면 틀 설정

--- electron/druid6/src/pages/server-monitoring/ServerMonitoring.jsx
+++ electron/druid6/src/pages/server-monitoring/ServerMonitoring.jsx
@@ -1,5 +1,4 @@
 import {useState} from 'react';
-import { Line } from "react-chartjs-2";
 import "./ServerMonitoring.scss";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';
@@ -65,12 +64,50 @@ export default function ServerMonitoring() {
 
     setNetworkHours(h);
   })
-  const data = {
+  const cpuData = {
     labels: ['used', 'unuse'],
     datasets: [
       {
         label: '# of Votes',
         data: [cpuUsage,100-cpuUsage],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(0, 0, 0, 0)',
+  
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+  const MemoryData = {
+    labels: ['used', 'unuse'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [MemoryUsage,100-MemoryUsage],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(0, 0, 0, 0)',
+  
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+  const DiskData = {
+    labels: ['used', 'unuse'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [DiskUsage,100-DiskUsage],
         backgroundColor: [
           'rgba(255, 99, 132, 0.2)',
           'rgba(0, 0, 0, 0)',
@@ -95,23 +132,32 @@ export default function ServerMonitoring() {
   return (
      <div className='ServerMonitoring'>
        <h1>ServerMonitoring</h1> 
-      <div className="Apap">
-       <Doughnut data={data} options={options} /> 
       
-       </div>
-
-      <div>프로세스 정보 : {processInfo}</div>
-      <div>운영체제 정보 : {osInfo}</div>
-      <div>설치된 램 : {ramInfo} gb</div>
-      <div>커널 릴리즈 버젼 : {kernelRelease}</div>
-      <div>커널 버젼 : {kernelVersion}</div>
-      <div>시스템 종류 : {systemInfo}</div>
-
+      <div className='badgeLong'>
+        <div>프로세스 정보 : {processInfo}</div>
+        <div>운영체제 정보 : {osInfo}</div>
+        <div>설치된 램 : {ramInfo} gb</div>
+        <div>커널 릴리즈 버젼 : {kernelRelease}</div>
+        <div>커널 버젼 : {kernelVersion}</div>
+        <div>시스템 종류 : {systemInfo}</div>
+      </div>
+      <div className='badgeContainer'>
+      <div className="badgeShort">
+       <Doughnut data={cpuData} options={options} /> 
+      </div>
+      <div className="badgeShort">
+       <Doughnut data={MemoryData} options={options} /> 
+      </div>
+      <div className="badgeShort">
+       <Doughnut data={DiskData} options={options} /> 
+      </div>
+      </div>
       <div>cpu사용량 : {cpuUsage}%</div>
       <div>memory사용량 : {MemoryUsage}%</div>
       <div>disk 사용량 : {DiskUsage}</div> 
       <div>실시간 트래픽 : {networkRealTime}</div>
       <div>하루 트래픽 : {networkHours}</div> 
+
     </div>
   );
 }
