001c0abb641997337e46dfd366c9cb8309eccf5b
Leo <guswhddlf11@gmail.com>
2022-05-15 20:41:39 +0900

    feat: 🎸 redux 초기설정 및 스트레스 테스트 UI
    TOO : 스트레스테스트 -> 시나리오테스트로 네이밍변경

--- electron/druid6/src/index.js
+++ electron/druid6/src/index.js
@@ -5,11 +5,16 @@ import App from './App';
 import reportWebVitals from './cra-utils/reportWebVitals';
 import "./static/fonts/font.css"
 
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store = { store }>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
