1071fe051e8a30118c88ed303088a81cfedf8e7f
kenny <kenny397@naver.com>
2022-05-17 10:40:37 +0900

    feat : dailyTraffic 실제값입력

--- electron/druid6/public/network.js
+++ electron/druid6/public/network.js
@@ -2,11 +2,15 @@ const network = (event,hostInfo,privateKeyPath) => {
     const Client = require('electron-ssh2').Client;
     
     const conn = new Client();
-    
+    let today= new Date();
+    let year = today.getFullYear();
+    let month = today.getMonth()+1;
+    if(month<10)month='0'+month;
     conn.on('ready', () => {
+     
       console.log('Client :: ready2');
       setTimeout(function (){
-        conn.exec(`vnstat -d`
+        conn.exec(`vnstat -d | grep ${year}-${month} | awk '{print $1,$2,$3,$5,$6}'`
         , (err, stream) => {
           if (err) throw err;
           stream.on('close', (code, signal) => {
