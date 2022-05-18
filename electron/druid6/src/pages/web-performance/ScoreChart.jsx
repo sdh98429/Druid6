9b01623ad178f7340c3b85a7c50e2ed426e64d25
SSAFY <sdh98429@naver.com>
2022-05-18 23:32:07 +0900

    feat: 🎸 NoUrl 페이지 추가, 웹퍼포먼스 css 배치 수정, 점수에 따라 그래프 색 변경, url 엔터

--- electron/druid6/src/pages/web-performance/ScoreChart.jsx
+++ electron/druid6/src/pages/web-performance/ScoreChart.jsx
@@ -1,32 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 export default function ScoreChart({ performanceScore, Color }) {
+  const [doughnutColorState, setDoughnutColorState] = useState(
+    "rgba(232, 232, 232, 1)"
+  );
+
+  const [grade, setGrade] = useState("X");
+
+  useEffect(() => {
+    if (performanceScore >= 90) {
+      setDoughnutColorState("rgba(0, 255, 0, 0.2)");
+      setGrade("A");
+    } else if (performanceScore >= 50) {
+      setDoughnutColorState("rgba(255, 120, 0, 0.2)");
+      setGrade("B");
+    } else {
+      setDoughnutColorState("rgba(255, 0, 0, 0.2)");
+      setGrade("C");
+    }
+  }, [performanceScore]);
+
   const data = {
     // labels: ["Red", "Blue"],
     datasets: [
       {
         // label: "# of Votes",
         data: [performanceScore, 100 - performanceScore],
-        backgroundColor: ["rgba(0, 255, 0, 0.2)", "rgba(232, 232, 232, 1)"],
+        // backgroundColor: ["rgba(0, 255, 0, 0.2)", "rgba(232, 232, 232, 1)"],
+        backgroundColor: [doughnutColorState, "rgba(232, 232, 232, 1)"],
         borderColor: ["rgba(255, 255, 255, 1)", "rgba(0, 255, 0, 0.2)"],
         borderWidth: 1,
         cutout: "80%",
       },
     ],
-  };
-
-  const grade = () => {
-    if (performanceScore >= 90) {
-      return "A";
-    } else if (performanceScore >= 50) {
-      return "B";
-    } else {
-      return "C";
-    }
   };
 
   return (
@@ -37,21 +47,19 @@ export default function ScoreChart({ performanceScore, Color }) {
         style={{
           position: "absolute",
           textAlign: "center",
-          // bottom: "10.5rem",
-          top: "31vh",
+          top: "28vh",
           left: "66.5vh",
           fontSize: "100px",
         }}
       >
-        {/* {performanceScore} */}
-        {grade()}
+        {grade}
       </div>
       <div
         className="performance-score-number"
         style={{
           position: "absolute",
           textAlign: "center",
-          top: "48vh",
+          top: "43vh",
           left: "67vh",
           fontSize: "50px",
         }}
