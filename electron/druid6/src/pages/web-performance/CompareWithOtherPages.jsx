504a1a44194ab8b434a44e61ecd32d9838471d3c
박윤지 <shaian2@naver.com>
2022-05-19 00:35:54 +0900

    style: ButtonToOtherPages 버튼 위치 정렬

--- electron/druid6/src/pages/web-performance/CompareWithOtherPages.jsx
+++ electron/druid6/src/pages/web-performance/CompareWithOtherPages.jsx
@@ -2,20 +2,23 @@ import "./CompareWithOtherPages.scss";
 import ButtonToOtherPages from "./ButtonToOtherPages";
 
 export default function CompareWithOtherPages() {
-  // const pageNames = ["myPage", "google", "naver", "daum", "bing"];
-
   return (
     <div className="CompareWithOtherPages">
-      <ButtonToOtherPages pageName={"myPage"} />
-      {/* <ButtonToOtherPages pageName={"google"} className="left-top" />
-      <ButtonToOtherPages pageName={"naver"} className="left-bottom" />
-      <ButtonToOtherPages pageName={"daum"} className="right-top" />
-      <ButtonToOtherPages pageName={"bing"} className="right-bottom" /> */}
-
-      {/* {pageNames.map((pageName) => (
-        <ButtonToOtherPages pageName={pageName} />
-      ))}
-      ; */}
+      <div className="center">
+        <ButtonToOtherPages pageName={"내 웹"} />
+      </div>
+      <div className="left-top">
+        <ButtonToOtherPages pageName={"구글"} />
+      </div>
+      <div className="left-bottom">
+        <ButtonToOtherPages pageName={"네이버"} />
+      </div>
+      <div className="right-top">
+        <ButtonToOtherPages pageName={"다음"} />
+      </div>
+      <div className="right-bottom">
+        <ButtonToOtherPages pageName={"빙"} />
+      </div>
     </div>
   );
 }
