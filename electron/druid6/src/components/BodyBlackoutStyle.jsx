d5d2f2e1af0ce03290eb45e0dc470629bae714c3
Leo <guswhddlf11@gmail.com>
2022-05-19 15:59:12 +0900

    feat: 🎸 로딩스피너 수정 및 메인페이지 수정

--- electron/druid6/src/components/BodyBlackoutStyle.jsx
+++ electron/druid6/src/components/BodyBlackoutStyle.jsx
@@ -4,7 +4,7 @@ import "./BodyBlackoutStyle.scss";
 export default function BodyBlackoutStyle() {
   return (
     <div className="body-blackout-style">
-      <p className="blackout-text">로딩중...</p>
+      <p className="blackout-text">테스트 중 입니다...</p>
       <img src="/images/giphy.gif" alt="" className="loading-spinner" />
     </div>
   );
